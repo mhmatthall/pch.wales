4d1171e0d8759efec4c120182f2ea64073f6ca6e
Matt Hall <matt@matthall.io>
2023-03-27 15:34:06 +0100

    Added middleware to make all URLs case-insensitive

---
+++ src/middleware.js
@@ -0,0 +1,30 @@
+// pch.wales middleware
+//    Makes all routes' URLs case-insensitive
+import { NextResponse } from "next/server";
+
+export function middleware(req) {
+  const urlLowercase = req.nextUrl.pathname.toLowerCase();
+
+  if (req.nextUrl.pathname === urlLowercase) {
+    return NextResponse.next();
+  } else {
+    return NextResponse.redirect(
+      new URL(req.nextUrl.origin + urlLowercase)
+    );
+  }
+};
+
+// ...with the exception of these
+export const config = {
+  matcher: [
+    /*
+     * Match all request paths except for the ones starting with:
+     * - api (API routes)
+     * - public (static files)
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - favicon.ico (favicon file)
+     */
+    '/((?!api|public|_next/static|_next/image|favicon.ico).*)',
+  ],
+};
