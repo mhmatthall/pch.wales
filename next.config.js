1f6d6c74a0284f8d2783069d2f5f0aed844d5d0e
Matt Hall <matt@matthall.io>
2023-03-27 14:33:51 +0100

    Migrated site redirects from hosting server

--- next.config.js
+++ next.config.js
@@ -1,6 +1,24 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+/**
+ * @type {import('next').NextConfig}
+ */
+module.exports = {
   reactStrictMode: true,
-}
+  async redirects() {
+    return [
+      {
+        source: '/dataprotection',
+        destination: 'https://www.swansea.ac.uk/about-us/compliance/data-protection/research-privacy-notice/',
+        basePath: false,
+        permanent: true,
+      },
+      {
+        source: '/data-protection',
+        destination: 'https://www.swansea.ac.uk/about-us/compliance/data-protection/research-privacy-notice/',
+        basePath: false,
+        permanent: true,
+      },
+    ]
+  },
+};
 
-module.exports = nextConfig
+// Note: must restart server on change
