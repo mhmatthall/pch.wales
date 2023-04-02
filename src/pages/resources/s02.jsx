5c45c37ea354671bb0a9b09bddeb026f30e3f843
Matt Hall <matt@matthall.io>
2023-04-03 00:42:24 +0100

    Add study downloads section

--- src/pages/resources/s02.jsx
+++ src/pages/resources/s02.jsx
@@ -16,6 +16,32 @@ export default function Page() {
       </Head>
       <div>
         Study S02
+        <div className={style.wide}>
+          <h1>
+            Downloads
+          </h1>
+          <p>
+            Download the supporting documents for this study here:
+          </p>
+          <ul className={style.fileList}>
+            <li className={style.fileCard}>
+              <Link href="/resources/s02/s02h02v03.pdf">
+                <img src="/resources/s02/s02h02v03.jpg" alt="Participant information sheet preview" />
+                <div className={style.fileName}>
+                  Participant information sheet
+                </div>
+              </Link>
+            </li>
+            <li className={style.fileCard}>
+              <Link href="/resources/s02/s02h01v01.pdf">
+                <img src="/resources/s02/s02h01v01.jpg" alt="Participant information sheet preview" />
+                <div className={style.fileName}>
+                  Consent form
+                </div>
+              </Link>
+            </li>
+          </ul>
+        </div>
       </div>
     </Layout>
   )
