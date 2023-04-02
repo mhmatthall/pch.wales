7ecf281cca1455619416ff65d283ed66e43a29d0
Matt Hall <matt@matthall.io>
2023-04-03 00:44:03 +0100

    Add S02 title section

--- src/pages/resources/s02.jsx
+++ src/pages/resources/s02.jsx
@@ -14,8 +14,44 @@ export default function Page() {
         <title>{"Understanding the individual's perspective of cancer care in South Wales | Study S02" + pageTitleSuffix}</title>
         <meta name="description" content="I think that there's room to make things better for those in South Wales undergoing cancer care – especially through thoughtfully-designed technology. I want to understand what cancer care receivers currently go through in South Wales and work with them to consider areas which could be improved as well as potential applications of technology which would enable more person-centred cancer care." />
       </Head>
+      <div className={style.contentRoot}>
+        <Link href="/resources">
+          &laquo; Back to Resources
+        </Link>
+        <div className={siteStyle.contentSection}>
+          <h1 className={style.title}>
+            Understanding the individual's perspective of cancer care in South Wales
+          </h1>
+          <div className={style.titleDetails}>
       <div>
+              Dec 2022&mdash;May 2023
+            </div>
+            <div className={style.hLine} />
+            <div className={style.right}>
         Study S02
+            </div>
+          </div>
+          <p>
+            I think that there&apos;s room to make things better for those in South Wales undergoing cancer care &ndash; especially through thoughtfully-designed technology. I want to understand what cancer care receivers currently go through in South Wales and work with them to consider areas which could be improved as well as potential applications of technology which would enable more person-centred cancer care.
+          </p>
+          <h3>
+            Research questions
+          </h3>
+          <ul>
+            <li>
+              What do those with cancer in South Wales experience on their treatment pathway?
+            </li>
+            <li>
+              What do they think about this pathway?
+            </li>
+            <li>
+              What does good quality-of-life look like to them in this situation?
+            </li>
+            <li>
+              How might they consider introducing technology into the pathway to make things better?
+            </li>
+          </ul>
+        </div>
         <div className={style.wide}>
           <h1>
             Downloads
