a3fa4c55839c2c64cee1d77e3a385f262ae89b85
Matt Hall <matt@matthall.io>
2023-04-03 09:38:39 +0100

    Fix broken rewrite condition -> /public/ doesn't exist in prod -> The rewriter is also implicitly causing our static file referencing issues

--- src/middleware.js
+++ src/middleware.js
@@ -20,11 +20,10 @@ export const config = {
     /*
      * Match all request paths except for the ones starting with:
      * - api (API routes)
-     * - public (static files)
      * - _next/static (static files)
      * - _next/image (image optimization files)
      * - favicon.ico (favicon file)
      */
-    "/((?!api|public|_next/static|_next/image|favicon.ico).*)",
+    "/((?!api|_next/static|_next/image|favicon.ico).*)",
   ],
 };
