f02d7b29280497d7e19202082d7d1e425666e1ca
Matt <42594962+mhmatthall@users.noreply.github.com>
2023-05-08 14:19:30 +0100

    Combine redirect rules for DPA policy

--- next.config.js
+++ next.config.js
@@ -6,19 +6,12 @@ module.exports = {
   async redirects() {
     return [
       {
-        source: '/dataprotection',
-        destination: 'https://www.swansea.ac.uk/about-us/compliance/data-protection/research-privacy-notice/',
+        source: "/(data-protection|dataprotection)",
+        destination:
+          "https://www.swansea.ac.uk/about-us/compliance/data-protection/research-privacy-notice/",
         basePath: false,
         permanent: true,
       },
-      {
-        source: '/data-protection',
-        destination: 'https://www.swansea.ac.uk/about-us/compliance/data-protection/research-privacy-notice/',
-        basePath: false,
-        permanent: true,
-      },
-    ]
+    ];
   },
 };
-
-// Note: must restart server on change
