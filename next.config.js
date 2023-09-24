7c4ff786b00ef24399f84fe549da595ac62c59cd
Matt <42594962+mhmatthall@users.noreply.github.com>
2023-09-24 14:48:51 +0100

    Add redirect for /go/s03-teams-msg

--- next.config.js
+++ next.config.js
@@ -12,6 +12,13 @@ module.exports = {
         basePath: false,
         permanent: true,
       },
+      {
+        source: "/go/s03-teams-msg",
+        destination:
+          "https://teams.microsoft.com/l/chat/0/0?users=matthew.hall3@wales.nhs.uk?message=Hi%20Matt!%20I%27m%20interested%20in%20your%20study.",
+        basePath: false,
+        permanent: true,
+      },
     ];
   },
 };
